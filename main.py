c7e0bf0092a0264bdff0cc64edaa161add794957
Nguyen Hau <nguyenhau442001@gmail.com>
2024-03-11 22:52:39 +0700

    Add edit box

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
- 
 # import module
 import streamlit as st
  
 # Title
-st.title("Hello GeeksForGeeks !!!")
+st.title("Summary Video Tool !!!")
+
+# Add an edit box
+user_input = st.text_input("Enter your text here:")
+st.write("You entered:", user_input)
